80ea6cc849dbce534b81f31ec8eb3d7cf1796874
Sikarev <roman.sikarev@kepler.team>
2023-04-28 18:12:06 +0300

    Top play section + top chart card

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,60 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+import { useGetTopChartsQuery } from '../redux/services/shazam-core';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+
+const TopChartCard = ({ song, i }) => (
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">{song.title}</div>
 );
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const wrapperRef = useRef(null);
+
+  useEffect(() => { wrapperRef.current.scrollIntoView({ behavior: 'smooth' }); });
+
+  const { data } = useGetTopChartsQuery();
+
+  const topPlays = data?.slice(0, 5);
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, index }));
+    dispatch(playPause(true));
+  };
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  return (
+    <div ref={wrapperRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+
+        <div className="mt-4 flex flex-col gap-1">
+          {topPlays?.map((song, i) => (<TopChartCard song={song} i={i} key={song.key} />))}
+        </div>
+
+        <div className="w-full flex flex-col mt-8">
+          <div className="flex flex-row justify-between items-center">
+            <h2 className="text-white font-bold text-2xl">Top Artists</h2>
+            <Link to="/top-artists">
+              <p className="text-gray-300 text-base cursor-pointer">See more</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TopPlay;
